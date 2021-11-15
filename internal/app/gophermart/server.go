1361c768ed31f8c8ed54cc7031db07153ef8356f
entertainment-veks <entertainment.veks@gmail.com>
2021-11-15 17:59:16 +0200

    implement accural system feature

--- internal/app/gophermart/server.go
+++ internal/app/gophermart/server.go
@@ -15,13 +15,13 @@ type server struct {
 	store  store.Store
 }
 
-func newServer(store store.Store) *server {
+func newServer(store store.Store, accuralSystemAddress string) *server {
 	s := &server{
 		router: mux.NewRouter(),
 		store:  store,
 	}
 
-	s.configureRouter()
+	s.configureRouter(accuralSystemAddress)
 
 	return s
 }
@@ -30,13 +30,13 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
-func (s *server) configureRouter() {
+func (s *server) configureRouter(accuralSystemAddress string) {
 	s.router.HandleFunc("/api/user/register", user.RegisterHandler(s.store)).Methods(http.MethodPost)
 	s.router.HandleFunc("/api/user/login", user.LoginHandler(s.store)).Methods(http.MethodPost)
 
 	private := s.router.NewRoute().Subrouter()
 	private.Use(user.AuthMiddleware)
-	private.HandleFunc("/api/user/orders", orders.OrdersPostHandler(s.store)).Methods(http.MethodPost)
+	private.HandleFunc("/api/user/orders", orders.OrdersPostHandler(s.store, accuralSystemAddress)).Methods(http.MethodPost)
 	private.HandleFunc("/api/user/orders", orders.OrdersGetHandler(s.store)).Methods(http.MethodGet)
 
 	private.HandleFunc("/api/user/balance", balance.BalanceHandler(s.store)).Methods(http.MethodGet)
